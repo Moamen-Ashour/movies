8a702696405bb675b6beb83b1425ac61ab16d085
Mo'men <codermanbrains@gmail.com>
2023-01-20 05:55:23 +0200

    - add layout

---
+++ lib/layout/layout.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Layout extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("layout"),
+      ),
+      body: Center(
+        child: Text("layout"),
+      ),
+    );
+  }
+}
